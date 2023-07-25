224e83c76f2952a9ec438148083ee781458fbf5c
nandinip92 <nandini.p8892@gmail.com>
2023-07-25 20:16:41 +0100

    feat: adding 'romanNumerals' dictionary

--- katas/roman_numerals/roman_numerals.js
+++ katas/roman_numerals/roman_numerals.js
@@ -1,3 +1,12 @@
 export function numbersToRomanNumerals(num) {
-  return;
+  const romanNumerals = {
+    1: "I",
+    5: "V",
+    10: "X",
+    50: "L",
+    100: "C",
+    500: "D",
+    1000: "M",
+  };
+  return romanNumerals[1];
 }
