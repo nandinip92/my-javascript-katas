8eb3341550b0a574393ad5f169e3cf3412347b1d
nandinip92 <nandini.p8892@gmail.com>
2023-07-26 09:59:02 +0100

    test: Added new test cases for integer >=4

--- katas/roman_numerals/roman_numerals.test.js
+++ katas/roman_numerals/roman_numerals.test.js
@@ -6,4 +6,8 @@ describe("numbersToRomanNumerals", () => {
     expect(numbersToRomanNumerals(2)).toBe("II");
     expect(numbersToRomanNumerals(3)).toBe("III");
   });
+  test("Returns a Roman Numaral of given ineger ( >=4 )", () => {
+    expect(numbersToRomanNumerals(4)).toBe("IV");
+    //expect(numbersToRomanNumerals(5)).toBe("V");
+  });
 });
