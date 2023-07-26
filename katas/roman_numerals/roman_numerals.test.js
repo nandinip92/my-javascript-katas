0e4e9146335cbe7fceddf7ff1ebb1346d9e1291f
nandinip92 <nandini.p8892@gmail.com>
2023-07-26 10:35:31 +0100

    tests: Adding testcases to the  roman numerals I,V,X,L,C,D,M

--- katas/roman_numerals/roman_numerals.test.js
+++ katas/roman_numerals/roman_numerals.test.js
@@ -1,13 +1,22 @@
 import { numbersToRomanNumerals } from "./roman_numerals";
 
 describe("numbersToRomanNumerals", () => {
+  test("Returns a Roman Numaral for 1,5,10,50,100,500,1000", () => {
+    expect(numbersToRomanNumerals(1)).toBe("I");
+    expect(numbersToRomanNumerals(5)).toBe("V");
+    expect(numbersToRomanNumerals(10)).toBe("X");
+    expect(numbersToRomanNumerals(50)).toBe("L");
+    expect(numbersToRomanNumerals(100)).toBe("C");
+    expect(numbersToRomanNumerals(500)).toBe("D");
+    expect(numbersToRomanNumerals(1000)).toBe("M");
+  });
   test("Returns a Roman Numaral of given ineger ( <=3 )", () => {
     expect(numbersToRomanNumerals(1)).toBe("I");
     expect(numbersToRomanNumerals(2)).toBe("II");
     expect(numbersToRomanNumerals(3)).toBe("III");
   });
-  test("Returns a Roman Numaral of given ineger ( >=4 )", () => {
+  test("Returns a Roman Numaral of given ineger ( >=4 and <=8)", () => {
     expect(numbersToRomanNumerals(4)).toBe("IV");
-    //expect(numbersToRomanNumerals(5)).toBe("V");
+    expect(numbersToRomanNumerals(5)).toBe("V");
   });
 });
