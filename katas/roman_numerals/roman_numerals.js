ef615ff7df5cd688d976c94170f581b426d7b8f7
nandinip92 <nandini.p8892@gmail.com>
2023-07-26 09:31:52 +0100

    feat: Added code to work with integers <5

--- katas/roman_numerals/roman_numerals.js
+++ katas/roman_numerals/roman_numerals.js
@@ -1,4 +1,10 @@
-export function numbersToRomanNumerals(num) {
+/* 
+googleclassroom link for the assignment: https://classroom.google.com/c/NTYzNzA2NDA5MDg2/a/NjE2NTcwODU4OTQw/details
+
+Source of this kata: https://codingdojo.org/kata/RomanNumerals/ 
+*/
+
+export function numbersToRomanNumerals(inputNumber) {
   // const romanNumerals = {
   // 1: "I",
   // 5: "V",
@@ -18,11 +24,29 @@ export function numbersToRomanNumerals(num) {
     M: 1000,
   };
 
-  const x = Object.keys(romanNumerals).find(
-    (key) => romanNumerals[key] === num
-  );
-  //console.log(x, typeof x);
-  return x;
+  let integerValues = Object.values(romanNumerals);
+  let resultStack = [];
+  //Checking if the number is greater than 5 or lessthan 5
+  if (inputNumber < 5) {
+    if (inputNumber == 4) {
+      resultStack.push(5, -1);
+    } else {
+      let sum = 0;
+      while (sum < inputNumber) {
+        sum += 1;
+        resultStack.push(1);
+      }
+    }
+  }
+  //console.log(resultStack);
+
+  const resultRomanArray = resultStack.map((num) => {
+    return Object.keys(romanNumerals).find((key) => romanNumerals[key] === num);
+  });
+
+  //console.log(resultRomanArray);
+
+  return resultRomanArray.join("");
 }
 
-numbersToRomanNumerals(1);
+//numbersToRomanNumerals(3);
