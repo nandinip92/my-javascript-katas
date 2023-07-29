ea5a69b76df8ec58dfcb6116b97ec41ba789e8f8
nandinip92 <nandini.p8892@gmail.com>
2023-07-29 06:10:03 +0100

    feat: Adding some code to start with converting roman numerals into numbers

---
+++ katas/roman_numerals/roman_numerals_to_numbers.js
@@ -0,0 +1,17 @@
+/* 
+googleclassroom link for the assignment: https://classroom.google.com/c/NTYzNzA2NDA5MDg2/a/NjE2NTcwODU4OTQw/details
+
+Source of this kata: https://codingdojo.org/kata/RomanNumerals/ 
+*/
+
+export function romanNumeralsToNumbers(inputRoman) {
+  const romanNumerals = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+}
